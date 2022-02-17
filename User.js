f12842983ffa0ddc3fab40da0496745db9c24efe
Alejandro España <alejo-e-1@hotmail.com>
2022-02-16 19:51:16 -0500

    Gestion esquema de usuarios mongoDb

--- User.js
+++ User.js
@@ -2,19 +2,73 @@ const mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 const addressSchema = new Schema({
-        street: String,
-        city: String
+    street: String,
+    city: String
 });
 
 const userSchema = new Schema({
     name: String,
-    age: Number,
-    email: String,
-    createdAT: Date,
-    updateAt: Date,
-    bestFriand: mongoose.SchemaTypes.ObjectId,
+    age: {
+        type: Number,
+        min: 1,
+        max: 100,
+        validate: {
+            validator: v => v % 2 === 0,
+            message: props => `${props.value} is not an even number`
+        }
+    },
+    email: {
+        type: String,
+        minlength: 10,
+        required: true,
+        lowercase: true
+    },
+    createdAT: {
+        type: Date,
+        default: new Date()
+    },
+    updateAt: {
+        type: Date,
+        immutable: true,
+        default: () => Date.now()
+    },
+    bestFriend: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: "User"
+    },
     hobbies: [String],
     address: addressSchema
 });
+
+userSchema.methods.sayHi = function() {
+    console.log(`Hi. My name is ${this.name}`);
+}
+
+userSchema.statics.findByName = function(name) {
+    return this.where({name: new RegExp(name,'i')})
+    // RegExp la i hace referencia a la búsqueda que no
+    // distingue entre mayúsculas y minúsculas.
+}
+
+userSchema.query.byName = function (name) {
+    return this.where({name: new RegExp(name,'i')})
+}
+
+userSchema.virtual('nameEmail').get(function() {
+    return`${this.name} <${this.email}>`
+})
+
+
+//Antes de guardar un usuario va actualizar la fecha de registro.
+userSchema.pre('save', function(next){
+    this.updateAt = Date.now()
+    next()
+})
+
+//Despues de guardar un usuario saluda.
+userSchema.post('save', function(doc,next){
+    doc.sayHi()
+    next()
+})
 
 module.exports = mongoose.model("User",userSchema);
--- script.js
+++ script.js
@@ -13,19 +13,46 @@ run();
 
 
 async function run() {
+
     try {
+        // Consulta por Id.
+        const userFind = await User.findById("620d8bc0656895d9c6bee7ae");
+        console.log(userFind);
+        // Consulta el primer registro que contenga el nombre Sally.
+        const userFindOne = await User.findOne({name: "Sally"});
+        console.log(userFindOne);
+        // Elimina un solo registro.
+        await User.deleteOne({name: "Alejandro"});
+        // Consulta usando Where.
+        const userWhere = await User.where("age")
+                                .gt(27)
+                                .lt(31)
+                                .where("name")
+                                .equals("Alejandro")
+                                .select("age")
+        console.log(userWhere);
+        // Creando un usuario
         const user = await User.create({
             name: "Alejo", 
-            age: 27,
+            age: 26,
+            email: "Prueba@gmail.com",
             hobbies: ["Weight lifting","Bowling"],
             address: {
                 street: "Main St",
                 city: "Colorado"
             }
         });
-        user.name = "Sally"
+        user.name = "Alejandro"
         await user.save()
         console.log(user)
+
+        //Utilizando metodos del esquema user.
+        console.log(user.sayHi())
+        console.log(await User.findByName("Alejandro"))
+        console.log(await User.find().byName("Alejandro"))
+        const userVirtual = await User.findOne({name:"Alejandro"});
+        console.log(userVirtual);
+        console.log(userVirtual.nameEmail);
     } catch (error) {
         console.log(error.message);
     }
